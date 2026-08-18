43c779bb3f14aacd89141125be358e651a99ca13
Rohit <vcc1664@gmail.com>
2026-08-18 11:24:59 +0530

    remove logo next js

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  devIndicators: false,
   allowedDevOrigins: ['10.79.214.88', 'localhost:3000', '127.0.0.1:3000'],
   reactCompiler: true,
   images: {
